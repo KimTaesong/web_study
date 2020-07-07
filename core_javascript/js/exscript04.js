d96a9e4c22ea198aee69838d8ce8fe93b0c5e8af
Taesong Kim <taesongweb@gmail.com>
2020-07-07 15:30:13 +0900

    study coreJavascript

---
+++ core_javascript/js/exscript04.js
@@ -0,0 +1,10 @@
+// alert 함수 
+alert('Hello');
+// prompt 함수 - 2개의 인수
+
+result = prompt(title, []);
+
+let age = prompt('나이를 입력해주세요.', 100);
+
+alert(`당신의 나이는 ${age}살 입니다.`);    // 당신의 나이는 100살입니다.
+
